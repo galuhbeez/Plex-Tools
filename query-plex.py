ec7080941817c27a6a8c358993b6a6f88495d9ad
Chris <chris@Chriss-MacBook-Pro.local>
2017-05-03 23:01:04 -0700

    updated to add sections and better xml parsing thanks to kim

--- query-plex.py
+++ query-plex.py
@@ -4,23 +4,24 @@ import xml.etree.ElementTree as ET
 plex_url =
 plex_token_qs =
 
-api_string = '/library/sections/'
-#api_string = '/library/sections/3/all'
+#api_string = '/library/sections/'
+api_string = '/library/sections/5/all'
 
 api_call = '%s%s%s' % (plex_url, api_string, plex_token_qs)
 
 print ('Calling %s' % (api_call))
 
 content = urllib2.urlopen(api_call).read()
-
+#
 root = ET.fromstring(content)
 
 # for child in root:
 #     print child.tag, child.attrib
 
-for child in root:
+for Part in root.iter('Part'):
+    # print Part.attrib
 #print child.tag, child.attrib
-    plex_titles = child.get('title')
+    plex_titles = Part.get('file')
     print plex_titles
 
 
